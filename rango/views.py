0d68c23c23e7522b749618942eb123bacf319074
“Zainab <zainabali@pc-131-100.customer.ask4.lan>
2023-02-06 22:59:16 +0000

    chapter 8 done

--- rango/views.py
+++ rango/views.py
@@ -45,7 +45,7 @@ def add_category(request):
         form = CategoryForm(request.POST)
         if form.is_valid():
             form.save(commit=True)
-            return redirect('/rango/')
+            return redirect(reverse('rango:index'))
         else:
             print(form.errors)
     return render(request, 'rango/add_category.html', {'form': form})
@@ -58,7 +58,7 @@ def add_page(request, category_name_slug):
         category = None
     
     if category is None:
-        return redirect('/rango/')
+        return redirect(reverse('rango:index'))
     
     form = PageForm()
     if request.method == 'POST':
